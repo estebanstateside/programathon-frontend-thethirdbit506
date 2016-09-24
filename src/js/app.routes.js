afd2ed999eb03a2f27daea0d7cc9e2a33eead706
Andres Gamboa <estebangam1@gmail.com>
2016-09-24 15:15:01 -0600

    new interceptor

--- src/js/app.routes.js
+++ src/js/app.routes.js
@@ -2,20 +2,20 @@
     'use strict';
     angular
         .module('pymeFbApp')
-        .config(function($locationProvider, $routeProvider) {
+        .config(function($locationProvider, $routeProvider, $httpProvider) {
             $routeProvider
                 .when('/inicio', {
                     templateUrl: 'views/login.html',
                     controller: 'LoginController',
                     controllerAs: 'login'
                 })
-                
+
                 .when('/registro', {
                     templateUrl: 'views/register.html',
                     controller: 'RegisterController',
                     controllerAs: 'register'
                 })
-                
+
                 .when('/administrador', {
                     templateUrl: 'views/dashboard.html',
                     controller: 'AdminController',
@@ -25,5 +25,21 @@
                     redirectTo: '/registro'
                 });
             $locationProvider.html5Mode(false);
+
+            $httpProvider.interceptors.push([
+              '$injector',
+              function ($injector) {
+                return $injector.get('AuthInterceptor');
+              }
+            ]);
+        })
+        .factory('AuthInterceptor', function ($rootScope, $q) {
+          return {
+            request: function (response) {
+              console.log('before sending');
+              console.log(response);
+              return response;
+            }
+          };
         });
 })();
