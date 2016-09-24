f4db3e08a1ba0f20ef9b39e1b2de355cf7417e56
carolina.l <carolina.l@wearestateside.com>
2016-09-24 13:12:36 -0600

    Added login view

--- src/js/app.config.js
+++ src/js/app.config.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
     angular
-        .module('app')
+        .module('pymeFbApp')
         .constant('config', {
             'title': 'La Voz del Cliente'
         });
