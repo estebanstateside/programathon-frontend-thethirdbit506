11b6ccc523a1454efd4520557da99d7e382e90c2
Mauricio Gamboa <magalocr@gmail.com>
2016-09-24 14:00:19 -0600

    Register Controller partial

---
+++ src/js/app.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('pymeFbApp')
+        .constant('constants', {
+            api: 'http://thethirdbit.space/index.php/rest'
+        });
+})();
