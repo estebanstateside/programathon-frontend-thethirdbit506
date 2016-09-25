36d53412d1ffac55b57227996a51c3836bf7b38f
Gabriel Garrido <gabrielgarrido@Gabriel.local>
2016-09-25 07:39:35 -0600

    results graphics

--- src/js/data/morris-data.js
+++ src/js/data/morris-data.js
@@ -64,14 +64,11 @@ $(function() {
     Morris.Donut({
         element: 'morris-donut-chart',
         data: [{
-            label: "Download Sales",
-            value: 12
+            label: "Mujeres",
+            value: 65
         }, {
-            label: "In-Store Sales",
-            value: 30
-        }, {
-            label: "Mail-Order Sales",
-            value: 20
+            label: "Hombres",
+            value: 35
         }],
         resize: true
     });
@@ -90,22 +87,6 @@ $(function() {
             y: '2008',
             a: 50,
             b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2010',
-            a: 50,
-            b: 40
-        }, {
-            y: '2011',
-            a: 75,
-            b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
         }],
         xkey: 'y',
         ykeys: ['a', 'b'],
