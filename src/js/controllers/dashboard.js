6dac637b45ac1d6e9f18bb39955f76d36243a66f
Gabriel Garrido <gabrielgarrido@Gabriel.local>
2016-09-25 07:01:57 -0600

    initalized results date field

--- src/js/controllers/dashboard.js
+++ src/js/controllers/dashboard.js
@@ -23,11 +23,32 @@
           }
         });
 
+
+
         dataService.getUser($rootScope.sessionData.UsuarioId).then(function(user){
             vm.user = user.data;
         });
 
         vm.title = 'Panel de Métricas';
+
+        vm.getCurrentDate = function() {
+            var today = new Date(),
+                dd = today.getDate(),
+                mm = today.getMonth() + 1,
+                yyyy = today.getFullYear();
+
+            if (dd < 10) {
+                dd = '0' + dd
+            }
+
+            if (mm < 10) {
+                mm = '0' + mm
+            }
+
+            today = dd + '/' + mm + '/' + yyyy;
+            console.log(today);
+            return today;
+        };
 
         vm.signOut = function() {
             sessionService.signOut(function() {
